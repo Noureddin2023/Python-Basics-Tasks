c42b775ac1dfdb272325742c3308c07121588318
Noureddin2023 <nam2021sa@gmail.com>
2022-11-06 16:53:38 +0100

    add Variables

---
+++ 01-Variables/Tasks.py
@@ -0,0 +1,63 @@
+# 1 : Create a Boolean variable nammed x
+x = False
+x = True
+# 2 : Create an integer variable named y
+y = 7
+y_x = 7
+y2 = 7
+# 3 : Create a float variable named z
+z = 5,5
+# 4 : Create a string variable names s
+s = 'nour'
+s = "nour"
+s = '''nour'''
+# 5 : Convert the int variable to float
+s = 12
+type(s)
+<class 'int'>
+s = 12/4
+type(s)
+<class 'float'>
+# 6 : Can we convert the str to int ?
+yes,we can:
+s = '50'
+s = int('50')
+type(s)
+<class 'int'>
+# 7 : Create a list of numbers from 1 to 5
+n = [1,2,3,4,5]
+# 8 : Create a tuple from 10 to 15
+n = (10,11,12,13,14,15)
+# 9 : Convert the list to tuple
+s = ['nour']
+s = ['nour'],['mohamed']
+type(s)
+<class 'tuple'>
+# 10 : Create a dict of 3 values
+n = {'nour':20 , 'mohamed':25 , 'mahmoud':30 }
+# 11 : Can we use semi colon; with python ?
+we can´t use the semi colon, In Python Semicoln is not used to denote the end of the line.
+# 12 : Python is interpreted or compiled ?
+Python is interpreted
+
+# 13 : What is the differences between low level & high level ?
+                                High Level                Low Level
+
+    Basic                    Programmer amiable         Machinefriendly
+          
+Speed of execution                Fast                       Slow
+
+  Translation                   Requires                   Assembler
+                       compiler or an interpreter     is required while machine language is directly executed
+                     
+Memory efficiency                Low                         High
+
+Comprehensibility           Understandable            Hard to understand
+
+ Portability and
+machine dependency     Portable and runnable in any  Nonprtable and machine dependent
+                           Platforms
+
+Debugging and                Simple                          Quite complex
+ maintenance
+
---
+++ 02-Operators/Tasks.py
@@ -0,0 +1,74 @@
+# 1 : What is the differences between = , ==
+'''
+    = : creat variable
+   == : Question
+'''
+# 2 : What do we mean by using !=
+'''
+   !=: not equal to operator
+'''
+# 3 : What is the operator precedence
+'''
+    operator precedence affects how an expression is evaluted and listed following:
+    ()
+    **
+    * /
+    + -
+'''
+# 4 : Create a variable x with value of 10
+'''
+    x = 10
+'''
+
+# 5 : Increase x value by 15 using assignment operators
+'''
+    x = x + 15
+    x
+    25
+'''
+
+# 6 : Divide the x value by 5 using assignment operators
+'''
+    x /= 5
+    x
+    2.0
+'''
+
+# 7 : Multiplay the x value by 10 using assignment operators
+'''
+    x*=10
+    x
+    100
+'''
+
+# 8 : Get module of x on 3 using assignment operators
+'''
+   x%=3
+   x
+   1
+'''
+
+# 9 : Using python print the module of 11 to 4
+'''
+
+'''
+
+# 10 : Print the exponent of 2,3
+'''
+     2**3
+      8
+'''
+
+# 11 : Divide 11 by 3 using python division
+'''
+     11/3
+     3.6666666666666665
+
+     11//3
+     3
+'''
+
+# 12 : Can we divide 11 by 3 and get an integer number?
+'''
+
+'''
