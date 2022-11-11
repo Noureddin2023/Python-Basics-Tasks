6edede0e559ef00d5919d58cab2431eac256bf29
Noureddin2023 <nam2021sa@gmail.com>
2022-11-11 21:37:16 +0100

    add Conditions

---
+++ 03-Conditions/Tasks.py
@@ -0,0 +1,71 @@
+# 1 : Check if 10 is bigger than 15 or not
+
+
+
+
+# 2 : If 10 is not bigger than 15 print x is smaller than 15
+
+
+
+
+# 3 : In wich cases we will use all
+
+
+
+
+# 4 : What is the differences between all , and
+
+
+
+
+# 5 : What is the differences between any , or
+
+
+
+
+# 6 : If we need all The conditions to be true we will use ....
+
+
+
+
+# 7 : What is the differences between if , elif
+
+
+
+
+# 8 : What is the differences between eilf , else
+
+
+
+
+# 9 : Can we use more than one elif
+
+
+
+
+# 10 : Can we use more than one else
+
+
+
+
+# 11 : write s simple ternary operator
+
+
+
+
+# 12 : in elif , python will check all the conditions or matter, what ?
+
+
+
+
+# 13 : in elif we us else for  ... ?
+
+
+
+
+# 14 : if we have this list [2,4,6,8,10]:
+         1. check to see if 4 in this list or not
+         2. check to see if 4 and 6 in this list or not
+         3. check to see if 3 or 6 in this list
+         4. check to see if 2,4 and 5 in this list 
+
