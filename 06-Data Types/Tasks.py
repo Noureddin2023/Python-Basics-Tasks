7a49f0faa9ed86cf430ef1d9618407a40a5cbf47
Noureddin2023 <nam2021sa@gmail.com>
2022-11-17 06:35:33 +0100

    add data types

---
+++ 06-Data Types/Tasks.py
@@ -0,0 +1,61 @@
+# 1 : With is the differences between '' "" ''' '''
+
+
+
+
+# 2 : Creat a string with the name 'mystro'
+
+
+
+
+# 3 : Make the string letters capital
+
+
+
+
+# 4 : Creat a list of values from 10 to 20
+
+
+
+
+# 5 : Add 30 to the end of the list
+
+
+
+
+# 6 : Add 40 as the second element of the list
+
+
+
+
+# 7 : Print how many elements in the list
+
+
+
+
+# 8 : replace the second element in the list with 100
+
+
+
+
+# 9 : Creat a tuple with values from 1 to 5
+
+
+
+
+# 10 : Can we add 10 to the end of the tuple?
+
+
+
+
+# 11 : Creat a dict of value Mahamoud:28 , ahmed:30
+
+
+
+
+# 12 : Print Mahmoud age from the dict
+
+
+
+
+# 13 : What is the differences between mutable and immutable data types?
