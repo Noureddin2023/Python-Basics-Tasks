8f6b2537e880131ae62b6cc3823bde0dd216c17f
Noureddin2023 <nam2021sa@gmail.com>
2022-11-17 17:58:19 +0100

    add data types

--- 06-Data Types/Tasks.py
+++ 06-Data Types/Tasks.py
@@ -1,61 +1,71 @@
 # 1 : With is the differences between '' "" ''' '''
-
-
+'''
+   '' and "" there is no difference between but used "" in the case of the Word with Apostrophes
+   ''' ''' used to leave a space between words or add a new  line
 
 
 # 2 : Creat a string with the name 'mystro'
 
+n='mystro'
 
 
 
 # 3 : Make the string letters capital
 
-
+n=('mystro')
+n.upper()
 
 
 # 4 : Creat a list of values from 10 to 20
-
+n=[10,11,12,13,14,15,16,17,18,19,20]
 
 
 
 # 5 : Add 30 to the end of the list
-
+n=[10,11,12,13,14,15,16,17,18,19,20]
+n.append(30)
 
 
 
 # 6 : Add 40 as the second element of the list
-
+n=[10,11,12,13,14,15,16,17,18,19,20]
+n.insert(1,40)
 
 
 
 # 7 : Print how many elements in the list
-
-
+n=[10,11,12,13,14,15,16,17,18,19,20]
+len(n)
 
 
 # 8 : replace the second element in the list with 100
-
+n=[10,11,12,13,14,15,16,17,18,19,20]
+n.replace(11,100)
 
 
 
 # 9 : Creat a tuple with values from 1 to 5
-
+n=[10,11,12,13,14,15,16,17,18,19,20]
+m=tuple(n)
 
 
 
 # 10 : Can we add 10 to the end of the tuple?
-
+we can not
 
 
 
 # 11 : Creat a dict of value Mahamoud:28 , ahmed:30
-
+n={'mahmoud:28,ahmed:30}
 
 
 
 # 12 : Print Mahmoud age from the dict
-
+n={'mahmoud:28,ahmed:30}
+n['mahmoud]
 
 
 
 # 13 : What is the differences between mutable and immutable data types?
+If the value can change, the object is called mutable, while if the value cannot change, the object is called immutable.
+'''
