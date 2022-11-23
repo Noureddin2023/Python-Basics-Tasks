15d00952d323516e1058b2faf4345655cbd58499
Noureddin2023 <nam2021sa@gmail.com>
2022-11-23 21:20:14 +0100

    add operators

--- 02-Operators/Tasks.py
+++ 02-Operators/Tasks.py
@@ -21,57 +21,56 @@
 '''
 
 # 5 : Increase x value by 15 using assignment operators
-'''
+
     x+=15
     x
     25
-'''
+
 
 # 6 : Divide the x value by 5 using assignment operators
-'''
+
     x /= 5
     x
     2.0
-'''
+
 
 # 7 : Multiplay the x value by 10 using assignment operators
-'''
+
     x*=10
     x
     100
-'''
+
 
 # 8 : Get module of x on 3 using assignment operators
-'''
+
    x%=3
    x
    1
-'''
+
 
 # 9 : Using python print the module of 11 to 4
-'''
+
      11%4
       3
-'''
 
 # 10 : Print the exponent of 2,3
-'''
+
      2**3
       8
-'''
+
 
 # 11 : Divide 11 by 3 using python division
-'''
+
      11/3
      3.6666666666666665
 
      11//3
      3
-'''
+
 
 # 12 : Can we divide 11 by 3 and get an integer number?
-'''
+
 yes
 
 11//3
-'''
+
