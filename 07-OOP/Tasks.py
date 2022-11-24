d04913a6dff2896cf7e2deb3660633d3eaeb2cee
Noureddin2023 <nam2021sa@gmail.com>
2022-11-24 21:51:22 +0100

    add oop

--- 07-OOP/Tasks.py
+++ 07-OOP/Tasks.py
@@ -1,5 +1,15 @@
 # 1 : Explain in few words what we mean by a class give and example
 '''
+A class is a code template for creating objects. Objects have member variables and have behaviour associated with them. In python a class is created by the keyword class . An object is created using the constructor of the class. This object will then be called the instance of the class
+
+Example: Apple : iphone 14:
+
+(Class) - Design:
+          - Color  Red , Gold
+          - Size    Normal Max
+(Object)- Production:
+          - Color  Gold
+          - Size   Normal Max
 
 '''
 
