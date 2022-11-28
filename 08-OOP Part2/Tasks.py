b9da271a3cea0cc6806d3a8c8caf8429aa514bf4
Noureddin2023 <nam2021sa@gmail.com>
2022-11-28 20:43:34 +0100

    addd oop2

---
+++ 08-OOP Part2/Tasks.py
@@ -0,0 +1,52 @@
+# 1. Create a new class names SciCalc with 3 methods , sum , mull , power all of them takes 2 argument x, y
+class SciValc:
+    def sum(x,y):
+        pass
+    def mull(x,y):
+        pass
+    def power(x,y)
+
+
+
+# 2. Sum return the sum of x and y
+
+
+
+
+
+# 3. Mull return the multiplication of x and y
+
+
+
+
+# 4. The power return x power y
+
+
+
+
+# 5. Take an object from the class and call the 3 methods with any numbers
+
+
+
+
+# 6. Can we inherit from the class we created in the first task Calc
+
+
+
+
+# 7. Inherit from the Calc class , now remove the unneeded code the the SciCalc after inheriting
+
+
+
+
+# 8. call the 3 methods again from the SciCalc object
+
+
+
+
+# 9. Now you should see the same result as before
+
+
+
+
+# 10. Explain in few words what happened after inheriting
