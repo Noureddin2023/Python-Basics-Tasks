8111791499bbb0dc2cfaa304f713fdc684d6bb67
Noureddin2023 <nam2021sa@gmail.com>
2022-11-30 19:52:26 +0100

    add oop3

--- 09-OOP Part3/Tasks.py
+++ 09-OOP Part3/Tasks.py
@@ -2,13 +2,29 @@
     # 1. Base class
 
 
+
+
     # 2. Parent class
    
 
+
+
     # 3. Sub class
+
+
 
 
     # 4. Sub class
 
 
+
+
     # 5. Derived class
+
+    
+
+# 2. Explain in few words what we mean with the word polymorphism , give an example    
+
+
+
+
