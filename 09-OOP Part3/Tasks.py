0274327853d6be956de7be69f2ed8836e837b0e1
Noureddin2023 <nam2021sa@gmail.com>
2022-11-30 20:12:06 +0100

    add oop3

--- 09-OOP Part3/Tasks.py
+++ 09-OOP Part3/Tasks.py
@@ -1,20 +1,23 @@
 # 1. What do we mean with the term :
     # 1. Base class
-
-
+    '''
+       a base class is an existing class from which the other classes are determined and properties are inherited.
+    '''
 
 
     # 2. Parent class
-   
-
+    '''
+      Parent class is the class being inherited from, also called base class. Child class is the class that inherits from another class, also called derived class.
+    '''
 
 
     # 3. Sub class
+    '''
+      is a class that derives from another class.
+    '''
 
 
-
-
-    # 4. Sub class
+    # 4. main class
 
 
 
